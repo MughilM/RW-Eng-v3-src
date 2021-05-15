b3465491811597d8202bb28289053a7345200350
MughilM <mughilan.muthupari@gmail.com>
2021-05-15 11:52:48 -0400

    models.py was in the wrong directory, so moved...

---
+++ event_rep/model_implementation/architecture/models.py
@@ -0,0 +1,10 @@
+"""
+File: event_rep/model_implementation/architecture/models.py
+Author: Mughilan Muthupari
+Creation Date: 2021-05-15
+
+This module holds all of the model classes that we utilize, starting from the
+original MTRFv4Res baseline model. Any extra models that are needed should
+be implemented here. Each model class should be paired with a hyperparameter class
+from hp/hyperparameters.py that define only the hyperparameters necessary.
+"""
--- event_rep/model_implementation/core/models.py
+++
@@ -1,9 +0,0 @@
-"""
-File: event_rep/model_implementation/core/models.py
-Author: Mughilan Muthupari
-Creation Date: 2021-05-15
-
-This module holds all of the model classes that we utilize, starting from the
-original MTRFv4Res baseline model. Any extra models that are needed should
-be implemented here.
-"""
