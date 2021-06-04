f62d16031a929ca8b563a5b6a073d6223974892a
MughilM <mughilan.muthupari@gmail.com>
2021-06-03 23:50:52 -0400

    Added the preprocessing for the thematic fit judgement correlation evaluation task.
    Signed-off-by: MughilM <mughilan.muthupari@gmail.com>

--- event_rep/evaluation/tasks.py
+++ event_rep/evaluation/tasks.py
@@ -8,6 +8,7 @@ functions, while the rest are subclassed.
 """
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 import os
 from typing import Dict, Type, Tuple
 from model_implementation.architecture.models import MTRFv4Res
@@ -15,7 +16,7 @@ from model_implementation.architecture.hp.hyperparameters import HyperparameterS
 
 
 class EvaluationTask:
-    def __init__(self, SRC_DIR, model_name, experiment_dir):
+    def __init__(self, SRC_DIR, model_name, experiment_name):
         # This is needed so the correct model structure is used
         # when loading the model from the checkpoint.
         PARAM_TO_MODEL: Dict[str, Type[MTRFv4Res]] = {
@@ -23,39 +24,38 @@ class EvaluationTask:
         }
         self.SRC_DIR = SRC_DIR
         self.model_name = model_name
-        self.experiment_dir = experiment_dir
+        self.experiment_name = experiment_name
         # Because this is a subclassed model we can't load the model all together,
         # due to unstable serialization for subclassed models. Instead, we can use
         # the checkpoints to load the weights. On the flip side, we have to make the
         # correct model structure, using the hyperparameters. This is why the model_name
         # is needed. The hyperparameters are saved in the experiment directory.
-        self.model_hp_set = HyperparameterSet(os.path.join(self.experiment_dir, 'hyperparameters.json'))
+        self.hp_set = HyperparameterSet(os.path.join(self.SRC_DIR, self.experiment_name, 'hyperparameters.json'))
         # Load the model using the hyperparameters
-        self.model: MTRFv4Res = PARAM_TO_MODEL[self.model_name](self.model_hp_set)
+        self.model: MTRFv4Res = PARAM_TO_MODEL[self.model_name](self.hp_set)
         print('Loaded model:')
         print(self.model.build().summary())
         # This should be used to save any metrics, that can later be written in
         # the generate report method.
         self.metrics = {}
 
-    def _preprocess(self) -> Tuple[Type[np.ndarray], Type[np.ndarray], Type[np.ndarray], Type[np.ndarray]]:
+    def _preprocess(self) -> Dict[str, np.ndarray]:
         """
         This function should output the correctly formatted numpy arrays
         that are designed to be passed in the model. Any variables that require state
         and need to be accessed when generating the report should be saved in metrics dictionary.
-        :return: A 4-tuple of np.arrays in the correct order, for passing through the model.
+        :return: A dictionary mapping the input layer names to the matrices.
         """
         raise NotImplementedError('Please implement a way to preprocess the data.')
 
-    def _calc_score(self, word_output, role_output):
+    def _calc_score(self, outputs: dict):
         """
         This internal method uses the output of the model to calculate the 'score' for
         the evaluation task, whether that be accuracy, correlation, or anything else.
         The score and anything related to it should be saved in the metrics dictionary for reading
         later and writing to a report.
         Ideally, the
-        :param word_output: The word outputs from the model.
-        :param role_output: The role outputs from the model.
+        :param outputs; The dictionary outputted from the model's call method.
         :return:
         """
         raise NotImplementedError('Please implement a way to calculate a score for this task.')
@@ -77,8 +77,66 @@ class EvaluationTask:
         method SHOULD NOT BE IMPLEMENTED in subclasses.
         :return:
         """
-        input_words, input_roles, target_word, target_role = self._preprocess()
-        word_outputs, role_outputs = self.model([input_words, input_roles, target_word, target_role])
-        self._calc_score(word_outputs, role_outputs)
+        all_inputs = self._preprocess()
+        outputs = self.model(all_inputs)
+        self._calc_score(outputs)
         self._generate_report()
 
+
+class CorrelateTFScores(EvaluationTask):
+    """
+    This class handles all the evaluation tasks where we are given a verb, noun, the role the
+    noun is supposed to fill, and a thematic fit judgement score. This data should be in
+    csv format with columns as verb,noun,role,score.
+    """
+    def __init__(self, SRC_DIR, model_name, experiment_name, dataset_name):
+        super().__init__(SRC_DIR, model_name, experiment_name)
+        self.dataset_input_file = os.path.join(self.SRC_DIR, f'evaluation/task_data/{dataset_name}.csv')
+        self.dataset = pd.read_csv(self.dataset_input_file)
+        self.dataset_ids = self.dataset.copy()
+
+    def _preprocess(self) -> Dict[str, np.ndarray]:
+        ROLE_MAP = {
+            'subj': 'A0',
+            'obj': 'A1',
+            'instrument': 'AM-MNR',
+            'location': 'AM-LOC'
+        }
+        # dataset_ids will have our words all be converted to IDs,
+        # including the roles, in the 3rd column. If we have
+        # a word that's not in the vocabulary, then put the unknown word ID.
+        # These are obtained from the hyperparameter set that was loaded.
+        self.dataset_ids[['verb', 'noun']] = self.dataset_ids[['verb', 'noun']]\
+            .apply(lambda word: self.hp_set.word_vocabulary.get(word, default=self.hp_set.unk_word_id))
+        # Convert our role column to role IDs, using the ROLE_MAP and the role vocabulary
+        self.dataset_ids['role'] = self.dataset_ids['role'].\
+            apply(lambda role: self.hp_set.role_vocabulary[ROLE_MAP[role]])
+        # Now we have our inputs in terms of IDs, and we can make the metrics.
+        # The important thing is that even though we only have 1 input word (the verb), the
+        # order must be preserved. To keep things simple, each input_role sample
+        # will be [0, 1, 2, 3, 4, 5]. Using the role vocabulary, we can get what
+        # ID the verb maps, and adjust that column.
+        input_roles = np.repeat(np.arange(6, dtype=int)[np.newaxis, :], repeats=self.dataset.shape[0], axis=0)
+        # First make the input_words be filled with missing words...
+        input_words = np.full(shape=(self.dataset.shape[0], 6), fill_value=self.hp_set.missing_word_id, dtype=int)
+        # This column will not change
+        verb_id = self.hp_set.role_vocabulary['V']
+        # Change that column in input_words...
+        input_words[:, verb_id] = self.dataset_ids['verb'].values
+        # Next, the target role is the role column...needs to have 1 column
+        target_role = self.dataset_ids['role'].values[:, np.newaxis]
+        # The target word doesn't matter, because we aren't worried about the predicted roles.
+        target_word = np.full(shape=(self.dataset.shape[0], 1), fill_value=1729, dtype=int)
+        # These are our inputs to the model.
+        return {
+            'input_roles': input_roles,
+            'input_words': input_words,
+            'target_role': target_role,
+            'target_word': target_word
+        }
+
+    def _calc_score(self, outputs):
+        pass
+
+    def _generate_report(self):
+        pass
