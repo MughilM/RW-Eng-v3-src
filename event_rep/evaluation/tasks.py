3a8daa3cfcca161f45f59f398a21ea83dd5c9fbc
MughilM <mughilan.muthupari@gmail.com>
2021-06-12 13:20:21 -0400

    Updated calculating score and generating the report for the correlation with the thematic fit judgements.
    Signed-off-by: MughilM <mughilan.muthupari@gmail.com>

--- event_rep/evaluation/tasks.py
+++ event_rep/evaluation/tasks.py
@@ -34,7 +34,7 @@ class EvaluationTask:
         self.hp_set = HyperparameterSet(os.path.join(self.EXPERIMENT_DIR, self.experiment_name, 'hyperparameters.json'))
         # Load the model using the hyperparameters
         self.model: MTRFv4Res = PARAM_TO_MODEL[self.model_name](self.hp_set)
-        print(f'Loaded model and hyperparameters from {self.EXPERIMENT_DIR}')
+        print(f'Loaded model and hyperparameters from {os.path.join(self.EXPERIMENT_DIR, self.experiment_name)}')
         # This should be used to save any metrics, that can later be written in
         # the generate report method.
         self.metrics = {}
@@ -78,7 +78,7 @@ class EvaluationTask:
         :return:
         """
         all_inputs = self._preprocess()
-        outputs = self.model(all_inputs)
+        outputs = self.model.predict(all_inputs)
         self._calc_score(outputs)
         self._generate_report()
 
@@ -89,7 +89,7 @@ class CorrelateTFScores(EvaluationTask):
     noun is supposed to fill, and a thematic fit judgement score. This data should be in
     csv format with columns as verb,noun,role,score.
     """
-    def __init__(self, SRC_DIR, EXPERIMENT_DIR, model_name, experiment_name, dataset_name):
+    def __init__(self, SRC_DIR, EXPERIMENT_DIR, model_name, experiment_name, dataset_name: str):
         super().__init__(SRC_DIR, EXPERIMENT_DIR, model_name, experiment_name)
         self.dataset_name = dataset_name
         self.dataset_input_file = os.path.join(self.SRC_DIR, f'evaluation/task_data/{dataset_name}.csv')
@@ -111,8 +111,6 @@ class CorrelateTFScores(EvaluationTask):
         # Doing both columns will not work, as we will run into the Series hash issue.
         self.dataset_ids['verb'] = self.dataset_ids['verb'].apply(word_to_id)
         self.dataset_ids['noun'] = self.dataset_ids['noun'].apply(word_to_id)
-        # self.dataset_ids[['verb', 'noun']] = self.dataset_ids[['verb', 'noun']]\
-        #     .apply(lambda word: self.hp_set.word_vocabulary.get(word, self.hp_set.unk_word_id))
         # Convert our role column to role IDs, using the ROLE_MAP and the role vocabulary
         self.dataset_ids['role'] = self.dataset_ids['role'].\
             apply(lambda role: self.hp_set.role_vocabulary[ROLE_MAP[role]])
@@ -141,16 +139,63 @@ class CorrelateTFScores(EvaluationTask):
         }
 
     def _calc_score(self, outputs):
-        word_out = outputs['w_out'].numpy()
+        word_out = outputs['w_out']
         # We want to see what are the word prediction probabilities for the words
         # in the 'word' column and correlate them with the thematic fit scores.
         # For now, we will just append the probabilities as a column, and
         # the report will generate metrics based on missing words.
         self.dataset['noun_probs'] = word_out[np.arange(self.dataset.shape[0]), self.dataset_ids['noun']]
+        # We will save the missing words, and calculate a correlation for t he whole
+        # dataset as well as a correlation with the missing words removed.
+        missing_verbs = self.dataset['verb'][self.dataset_ids['verb'] == self.hp_set.unk_word_id]
+        missing_nouns = self.dataset['noun'][self.dataset_ids['noun'] == self.hp_set.unk_word_id]
+        self.metrics['missing_verbs'] = np.unique(missing_verbs.values)
+        self.metrics['missing_nouns'] = np.unique(missing_nouns.values)
+        # Combine the two indices so we can easily remove from the main df in one line
+        total_missing_index = missing_verbs.index.union(missing_nouns.index)
+        self.metrics['missing_n'] = len(total_missing_index)
+        # Create a dataframe with the missing verbs + nouns dropped
+        self.dataset_dropped = self.dataset.drop(index=total_missing_index)
         rho, p = spearmanr(self.dataset['score'], self.dataset['noun_probs'])
         self.metrics['rho'] = rho
         self.metrics['p'] = p
+        # Calculate again with the dropped dataframe
+        rho, p = spearmanr(self.dataset_dropped['score'], self.dataset_dropped['noun_probs'])
+        self.metrics['rho_m'] = rho
+        self.metrics['p_m'] = p
 
     def _generate_report(self):
-        os.makedirs(os.path.join(self.EXPERIMENT_DIR, 'evaluation_results'))
-        print(self.metrics)
+        os.makedirs(os.path.join(self.EXPERIMENT_DIR, 'evaluation_results'), exist_ok=True)
+        # Short helper method to draw a box around a piece of text given padding...
+        make_box = lambda text, spaces: f"╔{'=' * (len(text) + 2 * spaces)}╗\n║{' ' * spaces}{text}{' ' * spaces}║\n" \
+                                        f"╚{'=' * (len(text) + 2 * spaces)}╝\n"
+        # Make the header
+        header = make_box(f'{self.dataset_name.upper()} RESULTS', 12) + '\n'
+
+        # Missing word label...
+        mv_report = make_box('MISSING WORD REPORT', 4)
+        mv_report += f"There are {len(self.metrics['missing_verbs'])} missing verbs and " \
+                   f"{len(self.metrics['missing_nouns'])} missing nouns for a total of " \
+                   f"{self.metrics['missing_n']} missing samples.\n\n"
+        # Apparently I can't put backslashes in f-strings :(
+        if len(self.metrics['missing_verbs']) > 0:
+            mv_report += f"MISSING VERBS\n{'=' * len(max(self.metrics['missing_verbs'], key=lambda x: len(x)))}\n"
+            mv_report += '\n'.join(self.metrics['missing_verbs']) + '\n'
+        if len(self.metrics['missing_nouns']) > 0:
+            mv_report += f"MISSING NOUNS\n{'=' * len(max(self.metrics['missing_nouns'], key=lambda x: len(x)))}\n"
+            mv_report += '\n'.join(self.metrics['missing_nouns']) + '\n'
+        mv_report += '\n'
+
+        # Final report on correlations...
+        fin_report = make_box('FINAL SPEARMAN CORRELATIONS', 6) + '\n'
+        # Multiply correlation by 100, round to 3 decimal places, round p-value to 6 decimal places
+        # Show number of samples in entire dataset and with missing samples removed.
+        fin_report += f"ENTIRE DATASET ({self.dataset.shape[0]}): {self.metrics['rho'] * 100:.3f}%, " \
+                      f"P-value of {self.metrics['p']:.6f}\n"
+        fin_report += f"MISSING REMOVED ({self.dataset_dropped.shape[0]}): {self.metrics['rho_m'] * 100:.3f}%, " \
+                      f"P-value of {self.metrics['p_m']:.6f}"
+        with open(os.path.join(self.EXPERIMENT_DIR, 'evaluation_results', f'{self.dataset_name}.txt'),
+                  'w', encoding='utf-8') as f:
+            f.write(header)
+            f.write(mv_report)
+            f.write(fin_report)
