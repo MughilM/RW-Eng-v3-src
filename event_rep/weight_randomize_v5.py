83cb8478974aa1acb6145e4d94266fc82d54b6f0
MughilM <mughilan.muthupari@gmail.com>
2022-01-08 15:33:47 -0500

    Finished randomization experiment where we randomize the network. Now to create the reverse, where we randomize the embeddings...

--- event_rep/weight_randomize_v5.py
+++ event_rep/weight_randomize_v5.py
@@ -19,6 +19,7 @@ Ideally, each model class would have a method which creates these two extra mode
 import inspect
 
 import numpy as np
+import pandas as pd
 import os
 import sys
 import logging
@@ -41,9 +42,6 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-# The experiment with the model we want to grab from
-EXPERIMENT_NAME = '3a_plus_03_v5_tunespacyavg_1perc'
-
 SRC_DIR = os.path.dirname(os.path.abspath(__file__))
 # Directory where experiments are saved
 EXPERIMENT_DIR = os.path.join(SRC_DIR, 'experiments')
@@ -54,106 +52,157 @@ PRETRAINED_DIR = os.path.join(SRC_DIR, 'pretrained_embeddings')
 CSV_PIECE_PATH = os.path.join(SRC_DIR, 'csv_piece_output')
 # Primary data path for data...
 DATA_PATH = os.path.join(SRC_DIR, 'processed_data')
-# Generate the huperparameter set from the SRC DIR
-hp_set = HyperparameterSet(os.path.join(EXPERIMENT_DIR, EXPERIMENT_NAME, 'hyperparameters.json'))
 
-# Create the model, and pull the weights from the latest checkpoint
-model_obj = MTRFv5Res(hp_set)
-model = model_obj.build_model(training=False, get_embedding=False)
-model.compile(optimizer=Adam(learning_rate=0.01), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# The experiment with the model we want to grab from
+EXPERIMENT_NAME = '3a_plus_03_v5_tunespacyavg_1perc'
 
-# Grab the checkpoint file
-checkpoint_dir = os.path.join(EXPERIMENT_DIR, EXPERIMENT_NAME, 'checkpoints')
-latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
-# Load the weights
-model.load_weights(latest_checkpoint).expect_partial()
-logger.info(f'Loaded model and hyperparameters from {os.path.join(EXPERIMENT_DIR, EXPERIMENT_NAME)}')
 
-# First create a map of the layer names to weights and biases
-# from the original model
-original_weights_biases = {}
-for i, layer in enumerate(model.layers):
-    original_weights_biases[layer.name] = {}
-    if len(layer.weights) == 0:
-        original_weights_biases[layer.name]['weights'] = None
-    else:
-        original_weights_biases[layer.name]['weights'] = layer.weights[0]
-    try:
-        original_weights_biases[layer.name]['bias'] = layer.bias.numpy()
-    except AttributeError:
-        original_weights_biases[layer.name]['bias'] = None
-    print('=' * 50)
-    print(layer.name, layer)
-    print('Weights:', layer.weights)
-    try:
-        print('Bias', layer.bias.numpy())
-    except AttributeError:
-        print('Bias', 'No Bias for this layer!')
+def randomize_network(experiment_name):
 
-print(original_weights_biases)
+    # Generate the huperparameter set from the SRC DIR
+    hp_set = HyperparameterSet(os.path.join(EXPERIMENT_DIR, experiment_name, 'hyperparameters.json'))
 
-# Now create map for the class attributes. We want to see what layer
-# name corresponds to what class attribute name. That way we can access the saem layer.
-# Unfortunately, the output names can have two different attributes, BUT we are in evaluation,
-# which means it will always be target_word_output_no_bias.
-layer_name_cls_att_map = {}
-for member in inspect.getmembers(model_obj):
-    # Remove private functions and object-level (__len__, __call__, etc.)
-    if not member[0].startswith('_') and not inspect.ismethod(member[1]) and issubclass(member[1].__class__, Layer):
-        if member[0] not in ['target_word_output', 'target_role_output']:
-            layer_name_cls_att_map[member[1].name] = member[0]
+    # Create the model, and pull the weights from the latest checkpoint
+    model_obj = MTRFv5Res(hp_set)
+    model = model_obj.build_model(training=False, get_embedding=False)
+    model.compile(optimizer=Adam(learning_rate=0.01), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-pprint.pprint(layer_name_cls_att_map)
+    # Grab the checkpoint file
+    checkpoint_dir = os.path.join(EXPERIMENT_DIR, experiment_name, 'checkpoints')
+    latest_checkpoint = tf.train.latest_checkpoint(checkpoint_dir)
+    # Load the weights
+    model.load_weights(latest_checkpoint).expect_partial()
+    logger.info(f'Loaded model and hyperparameters from {os.path.join(EXPERIMENT_DIR, experiment_name)}')
 
-print(model_obj.target_word_output_no_bias.weights[0].shape)
-print(model_obj.target_word_output_no_bias.weights[0].numpy())
-random_weights = np.random.random(size=(300, 50002))
-print('What the weights should be:', random_weights)
-l: Layer = getattr(model_obj, 'target_word_output_no_bias')
-l.set_weights([random_weights])
-print('What the weights are after attempted change')
-print(model_obj.target_word_output_no_bias.weights[0].numpy())
-
-# Go through each extracted layer in the model file,
-# and see if it has weights and biases. If they do, adjust the corresponding
-# layer in the model class. If it's an embedding layer, then copy it, otherwise,
-# put something random.
-print('Changing weights')
-print('===============================================')
-for layer_name, data in original_weights_biases.items():
-    print(layer_name)
-    weights = data['weights']
-    bias = data['bias']
-    weights_to_set = []
-    if layer_name not in layer_name_cls_att_map:
-        continue
-    l: Layer = getattr(model_obj, layer_name_cls_att_map[layer_name])
-    if weights is not None:
-        if 'embedding' in layer_name:
-            weights_to_set.append(weights.numpy())
+    # First create a map of the layer names to weights and biases
+    # from the original model
+    original_weights_biases = {}
+    for i, layer in enumerate(model.layers):
+        original_weights_biases[layer.name] = {}
+        if len(layer.weights) == 0:
+            original_weights_biases[layer.name]['weights'] = None
         else:
-            mini = np.min(weights.numpy())
-            maxi = np.max(weights.numpy())
-            random_weights = np.random.random(size=weights.numpy().shape) * (maxi - mini) + mini
-            weights_to_set.append(random_weights)
-    if bias is not None:
-        # Embeddings never have biases
-        mini = np.min(bias.numpy())
-        maxi = np.max(bias.numpy())
-        random_weights = np.random.random(size=bias.numpy().shape) * (maxi - mini) + mini
-        weights_to_set.append(random_weights)
-    # Now set the layer with the saved weights
-    if len(weights_to_set) > 0:
-        print('Weights before:', weights)
-        print('Min:', np.min(weights.numpy()))
-        print('Max:', np.max(weights.numpy()))
-        print('Weights after:', weights_to_set)
-        print('Min:', np.min(weights_to_set[0]))
-        print('Max:', np.max(weights_to_set[0]))
-        l.set_weights(weights_to_set)
+            original_weights_biases[layer.name]['weights'] = layer.weights[0]
+        try:
+            original_weights_biases[layer.name]['bias'] = layer.bias.numpy()
+        except AttributeError:
+            original_weights_biases[layer.name]['bias'] = None
+        # print('=' * 50)
+        # print(layer.name, layer)
+        # print('Weights:', layer.weights)
+        # try:
+        #     print('Bias', layer.bias.numpy())
+        # except AttributeError:
+        #     print('Bias', 'No Bias for this layer!')
 
-# Once we have set the correct weights, then build the model...
-new_model = model_obj.build_model(training=False, get_embedding=False)
+    print(original_weights_biases)
+
+    # Now create map for the class attributes. We want to see what layer
+    # name corresponds to what class attribute name. That way we can access the saem layer.
+    # Unfortunately, the output names can have two different attributes, BUT we are in evaluation,
+    # which means it will always be target_word_output_no_bias.
+    layer_name_cls_att_map = {}
+    for member in inspect.getmembers(model_obj):
+        # Remove private functions and object-level (__len__, __call__, etc.)
+        if not member[0].startswith('_') and not inspect.ismethod(member[1]) and issubclass(member[1].__class__, Layer):
+            if member[0] not in ['target_word_output', 'target_role_output']:
+                layer_name_cls_att_map[member[1].name] = member[0]
+
+    pprint.pprint(layer_name_cls_att_map)
+
+    # print(model_obj.target_word_output_no_bias.weights[0].shape)
+    # print(model_obj.target_word_output_no_bias.weights[0].numpy())
+    # random_weights = np.random.random(size=(300, 50002))
+    # print('What the weights should be:', random_weights)
+    # l: Layer = getattr(model_obj, 'target_word_output_no_bias')
+    # l.set_weights([random_weights])
+    # print('What the weights are after attempted change')
+    # print(model_obj.target_word_output_no_bias.weights[0].numpy())
+
+    # Go through each extracted layer in the model file,
+    # and see if it has weights and biases. If they do, adjust the corresponding
+    # layer in the model class. If it's an embedding layer, then copy it, otherwise,
+    # put something random.
+    print('Changing weights')
+    print('===============================================')
+    for layer_name, data in original_weights_biases.items():
+        print(layer_name)
+        weights = data['weights']
+        bias = data['bias']
+        weights_to_set = []
+        if layer_name not in layer_name_cls_att_map:
+            continue
+        l: Layer = getattr(model_obj, layer_name_cls_att_map[layer_name])
+        if weights is not None:
+            if 'embedding' in layer_name:
+                weights_to_set.append(weights.numpy())
+            else:
+                mean = np.mean(weights.numpy())
+                stdev = np.std(weights.numpy())
+                random_weights = np.random.normal(size=weights.numpy().shape, loc=mean, scale=stdev)
+                # random_weights = np.random.random(size=weights.numpy().shape) * (maxi - mini) + mini
+                weights_to_set.append(random_weights)
+        if bias is not None:
+            # Embeddings never have biases
+            mean = np.mean(weights.numpy())
+            stdev = np.std(weights.numpy())
+            random_weights = np.random.normal(size=weights.numpy().shape, loc=mean, scale=stdev)
+            # random_weights = np.random.random(size=bias.numpy().shape) * (maxi - mini) + mini
+            weights_to_set.append(random_weights)
+        # Now set the layer with the saved weights
+        if len(weights_to_set) > 0:
+            print('Weights before:', weights)
+            print('Mean:', np.mean(weights.numpy()))
+            print('Stdev:', np.std(weights.numpy()))
+            l.set_weights(weights_to_set)
+            print('Weights after:', getattr(model_obj, layer_name_cls_att_map[layer_name]).weights)
+            print('Mean:', np.mean(weights_to_set[0]))
+            print('Stdev:', np.std(weights_to_set[0]))
+
+
+    # Once we have set the correct weights, then build the model...
+    return model_obj
+    # new_model = model_obj.build_model(training=False, get_embedding=False)
+
+
+# Now, create the Evaluation Task classes, and provide this model
+# object to read from. Also say to NOT generate reports, since that would
+# override the original scores.
+task_order = ['ferretti_instrument', 'ferretti_location', 'greenberg', 'mcrae', 'pado', 'bicknell', 'gs']
+runs = 3
+randomization_results = pd.DataFrame(columns=task_order[:-1] + ['gs_full', 'gs_low', 'gs_high'],
+                                     index=[f'Run {run}' for run in range(1, runs + 1)])
+for run in range(1, runs + 1):
+    new_model = randomize_network(EXPERIMENT_NAME)
+    for task in task_order:
+        logger.info(f'Running {task}...')
+        if task == 'bicknell':
+            evaluator = BicknellTask(SRC_DIR, EXPERIMENT_DIR, 'v5', EXPERIMENT_NAME, model_obj=new_model,
+                                     write_report=False)
+        elif task == 'gs':
+            # Load the context embedding for the GS2013 task!!
+            evaluator = GS2013Task(SRC_DIR, EXPERIMENT_DIR, 'v5', EXPERIMENT_NAME, get_embedding=True,
+                                   model_obj=new_model, write_report=False)
+        else:
+            evaluator = CorrelateTFScores(SRC_DIR, EXPERIMENT_DIR, 'v5', EXPERIMENT_NAME, task,
+                                          model_obj=new_model, write_report=False)
+        evaluator.run_task()
+        # Once the task is done, the scores are saved in self.metrics, so we
+        # can just read the scores from there.
+        if task == 'bicknell':
+            logger.info(f'BICKNELL: {evaluator.metrics["accuracy"] * 100:.3f}%')
+            randomization_results.loc[f'Run {run}', 'bicknell'] = evaluator.metrics['accuracy']
+        elif task == 'gs':
+            logger.info(f'GS2013 (Full, Low, High): {evaluator.metrics["rho"] * 100:.3f}%, '
+                        f'{evaluator.metrics["low_rho"] * 100:.3f}%, {evaluator.metrics["high_rho"] * 100:.3f}')
+            randomization_results.loc[f'Run {run}', ['gs_full', 'gs_low', 'gs_high']] = \
+                [evaluator.metrics['rho'], evaluator.metrics['low_rho'], evaluator.metrics['high_rho']]
+        else:
+            logger.info(f'{task.upper()}: {evaluator.metrics["rho"] * 100:.3f}%')
+            randomization_results.loc[f'Run {run}', task] = evaluator.metrics['rho']
+
+# Write the randomization results to the
+randomization_results.to_csv(os.path.join(SRC_DIR, EXPERIMENT_DIR, EXPERIMENT_NAME, 'random_network_results.csv'))
 
 
 
